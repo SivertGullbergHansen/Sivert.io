ca0326084dace16faa7f4d788dcd53b118f6989f
Sivert <sivertgh@stud.ntnu.no>
2023-03-02 17:54:33 +0100

    Fixed framer motion

--- src/animations/Animations.ts
+++ src/animations/Animations.ts
@@ -1,24 +1,24 @@
 export const delayTime = .2
 
 export const top = {
-    initial: { opacity: 1, y: '-200px' },
+    initial: { opacity: .0001, y: '-200px' },
     animate: { opacity: 1, y: 0 },
     exit: { opacity: 0, y: '200px'},
 }
 export const bottom = {
-    initial: { opacity: 1, y: '200px' },
+    initial: { opacity: .0001, y: '200px' },
     animate: { opacity: 1, y: 0 },
     exit: { opacity: 0, y: '-200px'},
 }
 
 export const left = {
-    initial: { opacity: 1, x: '-200px' },
+    initial: { opacity: .0001, x: '-200px' },
     animate: { opacity: 1, x: 0 },
     exit: {opacity: 0, x: '200px'}
 }
 
 export const right = {
-    initial: { opacity: 1, x: '200px' },
+    initial: { opacity: .0001, x: '200px' },
     animate: { opacity: 1, x: 0 },
     exit: {opacity: 0, x: '-200px'}
 }
