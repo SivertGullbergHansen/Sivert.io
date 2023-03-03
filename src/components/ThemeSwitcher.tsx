16a6b71aca0fd16ea5dd7b9489bbafeda0d0438f
Sivert <sivertgh@stud.ntnu.no>
2023-03-03 21:22:39 +0100

    Cleanup

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,37 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from 'react'
-import { useTheme } from 'next-themes'
-import { BsFillLightbulbOffFill, BsFillLightbulbFill } from 'react-icons/bs'
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { BsFillLightbulbOffFill, BsFillLightbulbFill } from "react-icons/bs";
 
-const light = 'light'
-const dark = 'dark'
+const light = "light";
+const dark = "dark";
 
 const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-  
-function switchTheme() {
-  setTheme(theme === dark ? light : dark)
-}
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  function switchTheme() {
+    setTheme(theme === dark ? light : dark);
+  }
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
-    setMounted(true)
+    setMounted(true);
 
-    if (theme === 'system') setTheme(dark)
-  }, [])
+    if (theme === "system") setTheme(dark);
+  }, []);
 
   if (!mounted) {
-    return null
+    return null;
   }
 
   return (
-      <div onClick={switchTheme} className="flex flex-row gap-2 fixed top-4 left-4 place-items-center bg-base-300 px-1.5 py-1.5 rounded-full opacity-25 hover:opacity-100 hover:cursor-pointer">
-          <input type="checkbox" className="toggle" checked={theme === light} />
+    <div
+      onClick={switchTheme}
+      className="flex flex-row gap-2 fixed top-4 left-4 place-items-center bg-base-300 p-1.5 rounded-full opacity-25 hover:opacity-100 hover:cursor-pointer"
+    >
+      <input type="checkbox" className="toggle" checked={theme === light} />
       {theme !== light ? <BsFillLightbulbOffFill /> : null}
       {theme === light ? <BsFillLightbulbFill /> : null}
     </div>
-  )
-}
+  );
+};
 
-export default ThemeSwitch
+export default ThemeSwitch;
